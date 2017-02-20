68be4fca19595adfba5356da27a198c43a6ba555
Saurav Wahid <saurav1161@gmail.com>
2017-02-21 04:01:29 +0600

    21FEB2017 04:01 AM

--- src/main/java/org/javabase/apps/controller/CommentsController.java
+++ src/main/java/org/javabase/apps/controller/CommentsController.java
@@ -3,8 +3,10 @@
  */
 package org.javabase.apps.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.javabase.apps.entity.Comment;
@@ -15,6 +17,7 @@ import org.javabase.apps.service.ThreadService;
 import org.javabase.apps.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,7 +80,54 @@ public class CommentsController {
         }
         return response;
     }
+    @ResponseBody
+    @RequestMapping(value="/edit/update",method=RequestMethod.PUT)
+    public Map<String, Object> updateComment(@RequestBody Map<String, String> param,Model model){
+        Map<String, Object> response = new HashMap<>();
+        
+        Comment comment = new Comment();
+        Thread thread   = new Thread();
+        
+        String threadId = param.get("threadId");
+        String commentId = param.get("commentId");
+        thread.setThreadId(Integer.valueOf(threadId));
+        
+        comment.setCommentId(Integer.valueOf(commentId));
+        comment.setCommentDescription(param.get("commentDescription"));
+        comment.setCommentTitle(param.get("commentTitle"));
+        comment.setCreateUser(param.get("updateUser"));
+        comment.setThread(thread);
+        comment.setUpdateDate(new Date());
+        
+        boolean update = commentService.updateComment(comment);
+        if (update) {
+            response.put("suceess", true);
+            response.put("message", "Comment Edit");
+            response.put("path", "/thread/view/"+threadId);
+        }else {
+            response.put("error", true);
+            response.put("message", "Unable to Edit");
+        }
+        return response;
+    }
     
+    @RequestMapping(value="/edit/{commentId}_comment")
+    public String editThread(@PathVariable int commentId, Model model){
+        
+        Comment comment = commentService.getCommentbyId(commentId);
+        Thread thread = threadService.getThreadbyId(comment.getThread().getThreadId());
+        
+        List<Comment> commentList = new ArrayList<>();
+        if (thread !=null) {
+            commentList.addAll(thread.getComments());
+        }
+        
+        model.addAttribute("editCommnet", comment);
+        model.addAttribute("viewThread", thread);
+        model.addAttribute("commentList", commentList);
+        
+        return "view_thread";
+    }
     @RequestMapping(value="/delete/{commentId}_comment")
     public String deleteThread(@PathVariable int commentId){
         
--- src/main/webapp/resources/js/forum/comments.js
+++ src/main/webapp/resources/js/forum/comments.js
@@ -53,4 +53,57 @@ $(document).ready(function($) {
         });
         
     });
+    
+    //Comment Edit Function
+    $("#commentEditForm").submit(function(event) {
+        event.preventDefault();
+        
+        // Form validation use jquery validation plugin
+        javascript: $("#commentEditForm").validationEngine('attach');
+        if (!$("#commentEditForm").validationEngine('validate')) {
+            return;
+        }
+        
+        var data    = {},
+            url = "update";
+        data["commentDescription"]  = $("#commentDescriptionEdit").val();
+        data["updateUser"]          = $("#updateUser").val();
+        data["commentTitle"]        = $("#commentTitleEdit").val();
+        data["threadId"]            = $("#threadIdEdit").val();
+        data["commentId"]            = $("#commentId").val();
+        
+        var token     = $('#csrfToken').val();
+        var header    = $('#csrfHeader').val();
+        
+        /*
+         * if in spring aplication csrf enable
+         * send csrf parameter in header otherwise 405 error
+         */
+        $.ajax({
+            type      : "PUT",
+            url       : url,
+            data      : JSON.stringify(data),
+            dataType  : 'json',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Accept", "application/json");
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.setRequestHeader(header, token);
+            },
+            success  : function(resonse) {
+                var message = resonse.message;
+//                location.href = resonse;
+                document.getElementById("commentEditForm").reset()
+                $.notify(message, "success");
+            },
+            error      : function(e) {
+                console.log("ERROR: ",e);
+                $.notify("Unable to Post Comment", "error");
+            }
+        });
+        
+    });
+    $( "#commentEditLink a" ).click(function(e) {
+        e.preventDefault();
+        alert("click")
+      });
 });
